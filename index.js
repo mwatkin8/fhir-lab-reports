dc0dbcad0df776a55dede74a9fe03e153e2427a7
Michael Watkins <michael.watkins.8@gmail.com>
2019-08-30 13:38:26 -0600

    before reconfig

--- index.js
+++ index.js
@@ -7,24 +7,24 @@ let app = express();
 app.use(bodyParser.json());
 
 /**
- * Security Considerations:
- * - Must implement CORS in order to be called from a web browser
- */
+* Security Considerations:
+* - Must implement CORS in order to be called from a web browser
+*/
 app.use((request, response, next) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     response.setHeader('Access-Control-Allow-Credentials', 'true');
     response.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     response.setHeader('Access-Control-Expose-Headers', 'Origin, Accept, Content-Location, ' +
-        'Location, X-Requested-With');
+    'Location, X-Requested-With');
 
     // Pass to next layer of middleware
     next();
 });
 
 /**
- * Authorization.
- */
+* Authorization.
+*/
 app.use((request, response, next) => {
     // Always allow OPTIONS requests as part of CORS pre-flight support.
     if (request.method === 'OPTIONS') {
@@ -73,11 +73,11 @@ app.get('/lab', (request, response) => {
 
 //-------FOR HOOKS SERVICE-------
 /**
- * Discovery Endpoint:
- * - A GET request to the discovery endpoint, or URL path ending in '/cds-services'
- * - This function should respond with definitions of each CDS Service for this app in JSON format
- * - See details here: http://cds-hooks.org/specification/1.0/#discovery
- */
+* Discovery Endpoint:
+* - A GET request to the discovery endpoint, or URL path ending in '/cds-services'
+* - This function should respond with definitions of each CDS Service for this app in JSON format
+* - See details here: http://cds-hooks.org/specification/1.0/#discovery
+*/
 app.get('/cds-services', (request, response) => {
 
     let fhir_lab_reports = {
@@ -98,23 +98,27 @@ app.get('/cds-services', (request, response) => {
 });
 
 /**
- * Order Select Example Service:
- * - Handles POST requests to the order-select-example endpoint
- * - This function should respond with an array of cards in JSON format for the order-select hook
- *
- * - Service purpose: determine if the medication to be ordered is in our collection of blood pressure medications,
- * then warn the ordering clinician it may increase the patient's Framingham risk and link to our SMART app
- */
+* Order Select Example Service:
+* - Handles POST requests to the order-select-example endpoint
+* - This function should respond with an array of cards in JSON format for the order-select hook
+*
+* - Service purpose: determine if the medication to be ordered is in our collection of blood pressure medications,
+* then warn the ordering clinician it may increase the patient's Framingham risk and link to our SMART app
+*/
 app.post('/cds-services/fhir_lab_reports', (request, response) => {
     let implication = {};
-    for (let i = 0; i < request.body.prefetch.obs_pre.total; i++){
-        let r = request.body.prefetch.obs_pre.entry[i].resource;
-        let l = [];
-        r.component[0].valueCodeableConcept.coding.forEach(c => {
-            l.push(c.code);
-        });
-        implication[r.valueString] = l;
+    try{
+        for (let i = 0; i < request.body.prefetch.obs_pre.total; i++){
+            console.log(request.body.prefetch.obs_pre.entry.length);
+            let r = request.body.prefetch.obs_pre.entry[i].resource;
+            let l = [];
+            r.component[0].valueCodeableConcept.coding.forEach(c => {
+                l.push(c.code);
+            });
+            implication[r.valueString] = l;
+        }
     }
+    catch{}
     let cardValues = request.body.prefetch.hook.entry[0].resource;
     // Parse the request body for the FHIR context provided by the EHR. In this case, the MedicationRequest/MedicationOrder resource
     let draftOrder = request.body.context.draftOrders.entry[0].resource;
@@ -122,7 +126,7 @@ app.post('/cds-services/fhir_lab_reports', (request, response) => {
 
     // Check if a medication was chosen by the provider to be ordered
     if (['MedicationRequest', 'MedicationOrder'].includes(draftOrder.resourceType) && selections.includes(`${draftOrder.resourceType}/${draftOrder.id}`)
-        && draftOrder.medicationCodeableConcept) {
+    && draftOrder.medicationCodeableConcept) {
         const responseCard = createMedicationResponseCard(draftOrder, implication, cardValues); // see function below for more details
         response.send(JSON.stringify(responseCard, null, 2));
     }
@@ -130,10 +134,10 @@ app.post('/cds-services/fhir_lab_reports', (request, response) => {
 });
 
 /**
- * Creates a Card array based upon the medication chosen by the provider in the request context
- * @param context - The FHIR context of the medication being ordered by the provider
- * @returns {{cards: *[]}} - Either a card with the suggestion to switch medication or a textual info card
- */
+* Creates a Card array based upon the medication chosen by the provider in the request context
+* @param context - The FHIR context of the medication being ordered by the provider
+* @returns {{cards: *[]}} - Either a card with the suggestion to switch medication or a textual info card
+*/
 function createMedicationResponseCard(context, implication, cardValues) {
     let providerOrderedMedication = context.medicationCodeableConcept.coding[0].code;
     let indicator = 'info';
@@ -143,26 +147,26 @@ function createMedicationResponseCard(context, implication, cardValues) {
     let report_id = cardValues.action[0].action[0].prefix;
     for (let key in implication) {
         if (implication[key].includes(providerOrderedMedication)){
-        return {
-            cards: [
-                {
-                    summary: 'Risk detected',
-                    indicator: indicator,
-                    source: {
-                        "label": cardValues.action[0].documentation[0].label
-                    },
-                    links: [
-                        {
-                            label: cardValues.action[0].action[0].title,
-                            url: cardValues.action[0].action[0].code[0].text + '?reportID=' + report_id,
-                            type: cardValues.action[0].action[0].description
-                        }
-                    ],
-                    detail: key  + '\n\nUse the app to view the responsible lab report and additional details.'
-                }
-            ]
+            return {
+                cards: [
+                    {
+                        summary: 'Risk detected',
+                        indicator: indicator,
+                        source: {
+                            "label": cardValues.action[0].documentation[0].label
+                        },
+                        links: [
+                            {
+                                label: cardValues.action[0].action[0].title,
+                                url: cardValues.action[0].action[0].code[0].text + '?reportID=' + report_id,
+                                type: cardValues.action[0].action[0].description
+                            }
+                        ],
+                        detail: key  + '\n\nUse the app to view the responsible lab report and additional details.'
+                    }
+                ]
+            }
         }
-    }
     }
 }
 
